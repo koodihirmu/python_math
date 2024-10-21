14c329021ec692e1063ed141e5868a96e8777a23
koodihirmu <tomvar98@gmail.com>
2024-10-21 17:10:22 +0300

    fancier code

--- src/problem_2.py
+++ src/problem_2.py
@@ -13,7 +13,7 @@ class Triangle:
         self.hyp = hypothenusa
         self.side_a = side_a
         self.side_b = side_b
-        self.valid = False
+        self.valid = True
 
         # amount of known sides
         known_sides = 0
@@ -27,6 +27,7 @@ class Triangle:
         # check if input triangle is correct
         if known_sides < 2:
             print("Not enough known sides to calculate")
+            self.valid = False
             return
         elif known_sides > 2:
             print("Triangle is solved already")
@@ -36,34 +37,38 @@ class Triangle:
         # a²+b²=c²
         if self.hyp and self.side_a:
             self.side_b = np.sqrt(abs(np.square(self.hyp) - np.square(self.side_a)))
-            self.valid = True
             return
         if self.hyp and self.side_b:
             self.side_a = np.sqrt(abs(np.square(self.hyp) - np.square(self.side_b)))
-            self.valid = True
             return
         if self.side_a and self.side_b:
             self.hyp = np.sqrt(np.square(self.side_a) + np.square(self.side_b))
-            self.valid = True
             return
 
     def __del__(self) -> None:
         print("Triangle deleted")
 
     def print(self) -> None:
+        # check if the triangle is valid before printing
         if self.valid:
+            print("____________________________")
             print(f"side_a | {self.side_a:.2f}")
             print(f"side_b | {self.side_b:.2f}")
             print(f"hyp    | {self.hyp:.2f}")
+            print("____________________________")
         else:
             print("Invalid triangle")
 
 
 if __name__ == "__main__":
     triangle: List[Triangle] = [Triangle(2.3, 1.6)]
-    triangle.append(Triangle(2.3, None, 2.8))
-    triangle.append(Triangle(None, 1.6, 2.8))
-    triangle.append(Triangle(None, None, None))
+
+    # if we are in debug mode assign some more triangles
+    if __debug__:
+        triangle.append(Triangle(2.3, None, 2.8))
+        triangle.append(Triangle(None, None, None))
+        triangle.append(Triangle(2.3, 1.6, 2.8))
+
     for tri in triangle:
         tri.print()
     input("press enter ...")
