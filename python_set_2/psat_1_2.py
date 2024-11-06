488235b03a3662484907de53dd4ba445b2638fcb
Tomi <tomi@fedorakone.guests.metropolia.fi>
2024-11-06 14:19:28 +0200

    ready

--- python_set_2/psat_1_2.py
+++ python_set_2/psat_1_2.py
@@ -1,15 +1,31 @@
+import itertools as it
+from typing import List
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-fig = plt.figure(figsize=(3 * 6.4, 3 * 4.8), dpi=72)
+fig_scale = 2
+fig = plt.figure(figsize=(fig_scale * 6.4, fig_scale * 4.8), dpi=72)
 axs = plt.subplot()
 
+ticks = [0.0]
+names = ["0"]
+for i in range(1, 4):
+    ticks.append(-i * np.pi)
+    ticks.append(i * np.pi)
+    if i == 1:
+        names.append(f"-π")
+        names.append(f"π")
+    else:
+        names.append(f"-{i}π")
+        names.append(f"{i}π")
+
+axs.set_xlabel("radians")
+axs.set_ylabel("ratio")
+
 axs.set_xticks(
-    ticks=np.arange(-3 * np.pi, 3 * np.pi + np.pi / 4, np.pi / 4),
-    labels=[
-        f"{int(t/np.pi) if t % np.pi == 0 else int(t/(np.pi/4))/4}π" if t != 0 else "0"
-        for t in np.arange(-3 * np.pi, 3 * np.pi + np.pi / 4, np.pi / 4)
-    ],
+    ticks=ticks,
+    labels=names,
 )
 
 X = np.linspace(-3 * np.pi, 3 * np.pi, 256, endpoint=True)
